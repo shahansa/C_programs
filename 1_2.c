66c1425543fd58bda9497dab19d1af18faec5534
shehan492 <shehan492@outlook.com>
2017-05-25 14:34:10 +0530

    program to reverse a string in place terminated with NUL

---
+++ 1_2.c
@@ -0,0 +1,21 @@
+/*file : 
+  purpose
+*/
+#ifndef STRREV
+#define STRREV
+
+#include <stdio.h>
+#include <string.h>
+int reverseString(char *string)
+{
+	char temp;
+	int length=strlen(string);
+	if(length <=0 )
+	{
+		printf("\nERROR : wrong input\n");
+	}
+			
+}
+
+#endif
+
